09171c3d216bcd8999c21e305898d000edbea81e
Gaspar <gasparhe@gmail.com>
2025-05-19 20:48:33 +0200

    fix Product imports

--- src/app/components/confirm-delete-dialog/confirm-delete-dialog.component.ts
+++ src/app/components/confirm-delete-dialog/confirm-delete-dialog.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { Product } from '../../services/product.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-confirm-delete-dialog',
